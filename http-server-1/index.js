89adb0f2ceddb12071473b9159efb324ae489c3e
Samay Navale <92618552+SamayXD@users.noreply.github.com>
2023-12-15 00:59:56 +0530

    Express Basics

---
+++ http-server-1/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.post('/convo', function(req, res){
+let toSendMsg = "Hello " + req.body.msg + " with age " +req.body.age; 
+console.log("Response Sent: " + toSendMsg);
+res.send(toSendMsg);
+});
+
+app.get('/mobile', function(req, res){
+    let toSendMsgMobile = "HEllo to Mobile Samay"
+    res.send(toSendMsgMobile);
+    console.log("Response to Mobile: " + toSendMsgMobile)
+
+})
+
+app.listen(port,function(){
+    console.log("Open...")
+})
