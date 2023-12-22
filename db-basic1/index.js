8aa519f408a9b8b3de9e9fa8367e1bad6ca33694
Samay Navale <92618552+SamayXD@users.noreply.github.com>
2023-12-22 21:29:11 +0530

    ZOD

--- db-basic1/index.js
+++ db-basic1/index.js
@@ -5,22 +5,22 @@ const onPort = 3000;
 const schema = zod.array(zod.number());
 app.use(express.json());
 
-app.listen(onPort, function(){
-    console.log(`Opened on port ${onPort}`);
+app.listen(onPort, function() {
+  console.log(`Opened on port ${onPort}`);
 });
 
-app.post("/health-checkup", function(req, res){
-    const kidneys = req.body.kidneys;
-    const response = schema.safeParse(kidneys)
-    if(!response.success){
-        res.send({
-            response
-        })
-    }else{
-        res.send("DONE DONE");
-    }
+app.post("/health-checkup", function(req, res) {
+  const kidneys = req.body.kidneys;
+  const response = schema.safeParse(kidneys)
+  if (!response.success) {
+    res.send({
+      response
+    })
+  } else {
+    res.send("DONE");
+  }
 
-}); 
+});
 //global catches
 // app.use(function(err,req,res,next){
 //     res.json({
